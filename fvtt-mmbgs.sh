47e3afff3d41f15edabf75311faeaeac560e9a1b
Elmortti <eliot.fredman@gmail.com>
2024-08-03 02:40:27 +0300

    Added target folder functionality
    The modules are now created in the folder specified in the config.txt file.
    Removed single file version, as it will later be reworked and is rarely used.
    
    Reworking into a C++ application taken into consideration

--- fvtt-mmbgs.sh
+++ fvtt-mmbgs.sh
@@ -15,7 +15,8 @@ wait=$(grep "wait=.*" config.txt | sed 's/.*=//')
 scrt=$(grep "scrt=.*" config.txt | sed 's/.*=//')
 trgt=$(grep "trgt=.*" config.txt | sed 's/.*=//')
 log="$dt"".log"
-echo 'käytä komentoa -help nähdäksesi saatavilla olevat komennot'
+touch "$log"
+echo 'Use command -help to see available commands'
 read -p 'FVTT MMBGS: ' inp
 while [[ "$inp" =~ ^- ]]; do
 	if [[ "$inp" == "-help" || "$inp" == "-h" ]]
@@ -51,7 +52,7 @@ while [[ "$inp" =~ ^- ]]; do
 done
 if [[ $inp == $keyw ]]
 then
-	ln=$(wc -l "$bulk"/bulk.txt | grep -oE '[0-9]+')
+	ln=$(wc -l "$bulk"bulk.txt | grep -oE '[0-9]+')
 	let ln++
 	read -p "$ln modulia listassa, suorita? [Y]/[N]: " cnt
 	if [[ "$cnt" =~ ^N|^n ]]
@@ -62,16 +63,14 @@ then
 	while [[ $ln -gt 0 ]]
 	do
 		echo $ln jäljellä
-		let ln--
 		#define per-file variables
-		cur=$(head -n 1 "$bulk"/.txt | sed 's/\"/\\\\\"/g')
+		cur=$(head -n 1 "$bulk"bulk.txt | sed 's/\"/\\\\\"/g')
 		num=$(echo "$cur" | grep -Eo '^[0-9]+|[0-9]+$')
 		cur=$(echo "$cur" | sed -E "s/$num//g" | awk '{$1=$1};1')
-		sed -i '1d' "$bulk"/bulk.txt
 		lmap=${cur//[[:space:][:punct:]]/-}
 		lmap=${lmap,,}
 		lmap=$(echo "$lmap" | sed -E 's/-+/-/g' | sed -E 's/^-|-$//g')
-		imgs=$(find "$scrt" -type d -wholename *$num*/*$num*/*WEBP*)
+		imgs=$(find "$scrt" -type d -wholename "*$num*/*$num*/*WEBP*")
 		scn="miskasmaps-$num-$lmap"
 		if [[ "$imgs" =~ ^[[:space:]]*$|^$ ]]
 		then
@@ -79,31 +78,33 @@ then
 			then
 				echo "$num" "$cur"
 				read -p 'tiedostoja ei löytynyt, haluatko silti jatkaa?
-[Y] kyllä [A] kyllä kaikkiin [N] lopeta ohjelma [S] ohita tämä moduli: ' cnt
+	[Y] kyllä [A] kyllä kaikkiin [N] lopeta ohjelma [S] ohita tämä moduli: ' cnt
 			fi
 		fi
 		if [[ "$cnt" =~ ^N|^n ]]
 		then
 			exit
 		fi
+		let ln--
+		sed -i '1d' "$bulk"bulk.txt
 		#create directories and files
 		if ! [[ "$cnt" =~ ^S|^s ]]
 		then
-			mkdir "$scn"
-			cp -R "$dir"/template_module.json "$dir"/"$scn"/module.json
-			sed -i 's/%num%/'"$num"'/g' "$dir"/"$scn"/module.json
-			sed -i 's/%cur%/'"$cur"'/g' "$dir"/"$scn"/module.json
-			sed -i 's/%lmap%/'"$lmap"'/g' "$dir"/"$scn"/module.json
-			cd "$dir"/"$scn"
+			mkdir "$trgt"/"$scn"
+			cp -R "$dir"/template_module.json "$trgt"/"$scn"/module.json
+			sed -i 's/%num%/'"$num"'/g' "$trgt"/"$scn"/module.json
+			sed -i 's/%cur%/'"$cur"'/g' "$trgt"/"$scn"/module.json
+			sed -i 's/%lmap%/'"$lmap"'/g' "$trgt"/"$scn"/module.json
+			cd "$trgt"/"$scn"
 			mkdir artwork
 			mkdir tokens
 			mkdir audio
 			mkdir maps
 			mkdir tiles
 			#copy map image files into module
-			cp -R "$imgs" "$dir"/"$scn"/maps
-			mv "$dir"/"$scn"/maps/*WEBP*/* "$dir"/"$scn"/maps
-			rm -R "$dir"/"$scn"/maps/*WEBP
+			cp -R "$imgs" "$trgt"/"$scn"/maps
+			mv "$trgt"/"$scn"/maps/*WEBP*/* "$trgt"/"$scn"/maps
+			rm -R "$trgt"/"$scn"/maps/*WEBP
 		fi
 		cd $dir
 		if [[ "$imgs" =~ ^[[:space:]]*$^$ ]]
@@ -111,58 +112,18 @@ then
 			if [[ "$cnt" =~ ^S$|^s$ ]]
 			then
 				cnt=""
-				echo "Skipped $num $cur" > "$log"
+				echo "Skipped" "$num" "$cur" >> "$log"
 			else
-				echo "No files found $num $cur" > "$log"
+				echo "No files found" "$num" "$cur" >> "$log"
 			fi
 		else
-			echo "Processed $num $cur" > "$log"
+			echo "Processed" "$num" "$cur" >> "$log"
 		fi
 	done
-else
-	#single file version
-	cur=$(echo "$inp" | sed 's/\"/\\\\\"/g')
-	num=$(echo "$cur" | grep -Eo '^[0-9]+|[0-9]+$')
-	cur=$(echo "$cur" | sed -E "s/$num//g" | awk '{$1=$1};1')
-	lmap=${cur//[[:space:][:punct:]]/-}
-	lmap=${lmap,,}
-	lmap=$(echo "$lmap" | sed -E 's/-+/-/g' | sed -E 's/^-|-$//g')
-	imgs=$(find "$scrt" -type d -wholename *$num*/*$num*/*WEBP*)
-	scn="miskasmaps-$num-$lmap"
-	#create directories and files
-		if [[ "$imgs" =~ ^[[:space:]]*$|^$ ]]
-		then
-			echo "$num" "$cur"
-			read -p 'tiedostoja ei löytynyt, haluatko silti jatkaa?
-[Y] kyllä [N] lopeta ohjelma: ' cnt
-		fi
-		if [[ "$cnt" =~ ^N|^n ]]
-		then
-			exit
-		fi
-	#create directories and files
-	mkdir "$scn"
-	cp -R "$dir"/template_module.json "$dir"/"$scn"/module.json
-	sed -i 's/%num%/'"$num"'/g' "$dir"/"$scn"/module.json
-	sed -i 's/%cur%/'"$cur"'/g' "$dir"/"$scn"/module.json
-	sed -i 's/%lmap%/'"$lmap"'/g' "$dir"/"$scn"/module.json
-	cd ./"$scn"
-	mkdir artwork
-	mkdir tokens
-	mkdir audio
-	mkdir maps
-	mkdir tiles
-	#copy map image files into module
-	cp -R "$imgs" "$dir"/"$scn"/maps
-	mv "$dir"/"$scn"/maps/*WEBP*/* "$dir"/"$scn"/maps
-	rm -R "$dir"/"$scn"/maps/*WEBP*
-fi
-if [[ $inp == $keyw ]]
-then
 fi
 if [[ "$wait" == true ]]
 then
-	read -p "press enter" 
+	read -p "Press enter to exit" 
 else
 	true
 fi
